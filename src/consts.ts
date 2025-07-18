a4176c6e8479efecf89097290b483946fef2ca9a
Romero Hutapea <61641738+romerojhh@users.noreply.github.com>
2025-07-18 01:26:03 -0700

    update info

--- src/consts.ts
+++ src/consts.ts
@@ -56,26 +56,20 @@ export const SOCIALS: Socials = [
   { 
     NAME: "Email",
     ICON: "email", 
-    TEXT: "markhorn.dev@gmail.com",
-    HREF: "mailto:markhorn.dev@gmail.com",
+    TEXT: "romerojhh@gmail.com",
+    HREF: "mailto:romerojhh@gmail.com",
   },
   { 
     NAME: "Github",
     ICON: "github",
-    TEXT: "markhorn-dev",
-    HREF: "https://github.com/markhorn-dev/astro-sphere"
+    TEXT: "romerojhh",
+    HREF: "https://github.com/romerojhh"
   },
   { 
     NAME: "LinkedIn",
     ICON: "linkedin",
-    TEXT: "markhorn-dev",
-    HREF: "https://www.linkedin.com/in/markhorn-dev/",
-  },
-  { 
-    NAME: "Twitter",
-    ICON: "twitter-x",
-    TEXT: "markhorn_dev",
-    HREF: "https://twitter.com/markhorn_dev",
-  },
+    TEXT: "romerojhh",
+    HREF: "https://www.linkedin.com/in/romerojhh/",
+  }
 ]
 
